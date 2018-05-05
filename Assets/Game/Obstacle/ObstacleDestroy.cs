236055620cff676cf95d10cd4d77793b697c73de
Cat Flynn <mewtwo97@hotmail.com>
2018-05-06 00:19:40 +0100

    Destory blocks behind player

---
+++ Assets/Game/Obstacle/ObstacleDestroy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleDestroy : MonoBehaviour
+{
+    [SerializeField]
+    private float _minZ;
+    
+    void Update()
+    {
+        if (transform.position.z < _minZ)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
