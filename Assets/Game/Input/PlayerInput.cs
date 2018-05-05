42496c25cef72a79bbf6693fd35d4153ff3bcdfe
U-UNIVERSITY\100458008 <100458008@40167EA45E2B.university.ds.derby.ac.uk>
2018-05-05 13:58:31 +0100

    Add input, game tags

---
+++ Assets/Game/Input/PlayerInput.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public float HorizontalAxis
+    {
+        get
+        {
+            return Input.GetAxis("Horizontal");
+        }
+    }
+
+    void Update()
+    {
+        print(HorizontalAxis);
+    }
+}
