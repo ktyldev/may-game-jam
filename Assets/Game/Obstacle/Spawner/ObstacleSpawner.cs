9ac78b806e5939da7960bcb24c62e5b9a983f0ca
Cat Flynn <mewtwo97@hotmail.com>
2018-05-05 21:28:32 +0100

    Basic obstacle spawner

---
+++ Assets/Game/Obstacle/Spawner/ObstacleSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _obstacle;
+
+    [SerializeField]
+    private float _delay;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(SpawnObstacles());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private IEnumerator SpawnObstacles()
+    {
+        var count = 10;
+
+        for (int i = 0; i < count; i++)
+        {
+            Instantiate(_obstacle, transform.position, transform.rotation);
+            yield return new WaitForSeconds(_delay);
+        }
+    }
+}
