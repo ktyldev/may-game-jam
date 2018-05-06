c3e02b09b06c9a7a0ca278eb513218174b223e6c
Dogelix <yeatmanjake@gmail.com>
2018-05-06 11:43:58 +0100

    Added some patterns

--- Assets/Game/Obstacle/PatternEnd.cs
+++ Assets/Game/Obstacle/PatternEnd.cs
@@ -10,18 +10,21 @@ public class PatternEnd : MonoBehaviour
 
     private ShipMovement _ship;
     private Score _score;
+    private UI_Controller _ui;
 
     void Awake()
     {
         PatternComplete = new UnityEvent();
         _ship = this.Find<ShipMovement>(GameTags.Player);
         _score = this.Find<Score>(GameTags.Score);
+        _ui = this.Find<UI_Controller>(GameTags.IG_GUI);
     }
 
     void OnDestroy()
     {
         _score.IncrementScoreMultiplier();
         _ship.IncrementSpeed();
+        _ui.DisplayStage(true);
         PatternComplete.Invoke();
     }
 }
