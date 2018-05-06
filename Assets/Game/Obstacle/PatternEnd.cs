7ead70899f27d8e72ef3abaebe20747e3d7aee2c
Cat Flynn <mewtwo97@hotmail.com>
2018-05-06 09:11:55 +0100

    Pattern end

---
+++ Assets/Game/Obstacle/PatternEnd.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PatternEnd : MonoBehaviour
+{
+    public UnityEvent PatternComplete { get; private set; }
+
+    void Awake()
+    {
+        PatternComplete = new UnityEvent();
+    }
+
+    void OnDestroy()
+    {
+        PatternComplete.Invoke();
+    }
+}
